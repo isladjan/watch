dd235e279004292fbef3c585279923841ef61787
isladjan <isladjan83@gmail.com>
2023-02-08 00:25:36 +0100

    t2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let infoDiv = document.querySelector(".info");
-let text = "Total width/height: " + screen.width + "*" + screen.height + "<br>" +
-"Available width/height: " + screen.availWidth + "*" + screen.availHeight + "<br>" +
-"Color depth: " + screen.colorDepth + "<br>" +
-"Color resolution: " + screen.pixelDepth;
-infoDiv.innerHTML = text;
+// let infoDiv = document.querySelector(".info");
+// let text = "Total width/height: " + screen.width + "*" + screen.height + "<br>" +
+// "Available width/height: " + screen.availWidth + "*" + screen.availHeight + "<br>" +
+// "Color depth: " + screen.colorDepth + "<br>" +
+// "Color resolution: " + screen.pixelDepth;
+// infoDiv.innerHTML = text;
 
 
 
